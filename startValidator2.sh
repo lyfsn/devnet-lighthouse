eab7c49aac3eaf7a8ec184770ae1b3c297f9bee6
wangyufsn <dev.wangyu@proton.me>
2023-11-20 17:40:30 +0800

    up

--- startValidator2.sh
+++ startValidator2.sh
@@ -8,7 +8,7 @@ lighthouse validator_client \
   --unencrypted-http-transport \
   --http-address=0.0.0.0 \
   --http-port=5142 \
-  --beacon-nodes=http://127.0.0.1:4100 \
+  --beacon-nodes=http://127.0.0.1:4109 \
   --suggested-fee-recipient=0x8943545177806ED17B9F23F0a21ee5948eCaa776 \
   --metrics \
   --metrics-address=0.0.0.0 \
